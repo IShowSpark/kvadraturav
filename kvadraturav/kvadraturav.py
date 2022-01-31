017925ce11e210dc0e3dbf33ba1e5dd480d07a75
opilane <opilane@HP-CZC6297BB7>
2022-01-31 10:42:57 +0200

    sdadsad

--- kvadraturav/kvadraturav.py
+++ kvadraturav/kvadraturav.py
@@ -1,121 +1,100 @@
 from tkinter import *
-from math import sqrt
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
 
-def answer(a,b,c):
-    D=b*b-4*a*c
-    if D>=0:
-        x1=(-b+sqrt(D))/(2*a)
-        x2=(-b-sqrt(D))/(2*a)
-        text="Дискриминант:%s\nX1: %s\nX2: %s\n"%(D,x1,x2)        
+
+def ochko():
+    x1=np.arrange(-9,-1.5,0.5)
+    y1=(-1/16)*(x+5)**2+2
+    x2=np.arrange(1,9.5,0.5)
+    y2=(-1/16)*(x-5)**2+2
+    x3=np.arrange(-9,-1.5,0.5)
+    y3=1/4*(x+5)**2-3
+    x4=np.arrange(1,9.5,0.5)
+    y4=1/4*(x-5)**2-3
+    x5=np.arrange(-9,-6.5,0.5)
+    y5=-(x+7)**2+5
+    x6=np.arrange(6,9.5,0.5)
+    y6=-(x-7)**2+5
+    x7=np.arrange(-1,1.5,0.5)
+    y7=-0,5**2+1,5
+    fig=plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title("Очки")
+    plt.ylabel("y")
+    plt.xlabel("x")
+    plt.grid(True)
+    plt.show()
+
+def handler():    
+    if (a.get()!="" and b.get()!="" and c.get()!=""):
+        a1=int(a.get())
+        b1=int(b.get())
+        c1=int(c.get())
+        D=b1*b1-4*a1*c1
+        if D>0:
+            xround=round((-1*b1+sqrt(D))/(2*a1),2)
+            xround1=round((-1*b1-sqrt(D))/(2*a1),2)
+            t=f"X1={xround} \nX2={xround1}"
+            flag=True
+        elif D==0:
+            xround=round((-1*b1)/(2*a1),2)
+            t=f"X1={xround}"
+            flag=True
+        else:
+            t="Корней нет"
+            flag=False
+        answer.configure(text=f"D={D}\n{t}")
+    return flag,D,t
+def graafik():
+    flag,D,t=handler()
+    if flag==True:
+        a1=int(a.get())
+        b1=int(b.get())
+        c1=int(c.get())
+        x0=(-b1)/(2*a1)
+        y0=a1*x0*x0+b1*x0+c1
+        x = np.arange(x0-10, x0+10, 0.5)
+        y=a1*x*x+b1*x+c1
+        fig = plt.figure()
+        plt.plot(x, y,'b:o', x0, y0,'g-d')
+        plt.title('Квадратное уравнение')
+        plt.ylabel('y')
+        plt.xlabel('x')
+        plt.grid(True)
+        plt.show()
+        text=f"Вершина параболлы ({x0},{y0})"
     else:
-        text="Дискриминант:%s\n Это уравнение не имеет решений"%D 
-    return text
-
-def inserter(value):
-    label5.delete("0.0","end")
-    label5.insert("0.0",value)
-
-
-def handler():
-    try:
-        a_vibor=float(a.get())
-        b_vibor=float(b.get())
-        c_vibor=float(c.get())
-        inserter(answer(a_vibor,b_vibor,c_vibor))
-    except ValueError:
-        inserter("Надо ввести всего 3 числа")
-    
-    
-
-
-
-
-
+        text=f"График нет возможности построить"
+    answer.configure(text=f"D={D}\n{t}\n{text}")
 window=Tk()
-window.resizable(width=False, height=False)
-window.minsize(800,300)
-
+window.geometry("640x240")
 window.title("Квадратные уравнения")
-button1=Button(window,text="Решить",font="Arial 20",height=2,bg="white",fg="#00BFFF",relief=SOLID,command=handler)
-button1.place(x=480,y=80)
-button2=Button(window,text="График",font="Arial 20",height=2,bg="white",fg="#00BFFF",relief=SOLID,command=handler)
-button2.place(x=600,y=80)
+f1=Frame(width=640,h)
+f2=Frame(width=640,height=)
 
 
-label1=Label(window,text="Решение квадратного уравнения",height=2,width=40,font="Arial 20",bg="white",fg="#00BFFF",relief="solid")
-label1.pack()
-a=Entry(window,font="Arial 30",width=3,bg="white",fg="#00BFFF",justify=CENTER)
-a.place(x=100,y=100)
-label2=Label(window,text="x**2+",height=3,font="Arial 20",fg="#00BFFF")
-label2.place(x=180,y=75)
-b=Entry(window,font="Arial 30",width=3,bg="white",fg="#00BFFF",justify=CENTER)
-b.place(x=260,y=100)
-label3=Label(window,text="x+",height=3,font="Arial 20",fg="#00BFFF")
-label3.place(x=320,y=75)
-c=Entry(window,font="Arial 30",width=3,bg="white",fg="#00BFFF",justify=CENTER)
-c.place(x=360,y=100)
-label4=Label(window,text="=0",height=3,font="Arial 20",fg="#00BFFF")
-label4.place(x=420,y=75)
-label5=Text(window,height=4,width=40,font="Arial 20",bg="white",fg="#00BFFF")
-label5.place(x=80,y=180)
+label=Label(window,text="Решение квадратного уравнения",font="Calibri 26", fg="black",bg="white")
+label.pack()
+answer=Label(window,height=4,width=60,bg="white")
+answer.pack(side=BOTTOM)
+a=Entry(window,font="Calibri 26",fg="black",bg="white",width=3)
+a.pack(side=LEFT)
+x2=Label(window,text="x**2+",font="Calibri 26",fg="black")
+x2.pack(side=LEFT)
+b=Entry(window,font="Calibri 26",fg="black",bg="white",width=3)
+b.pack(side=LEFT)
+x=Label(window,text="x+",font="Calibri 26",fg="black")
+x.pack(side=LEFT)
+c=Entry(window,font="Calibri 26",fg="black",bg="white",width=3)
+c.pack(side=LEFT)
+y=Label(window,text="=0",font="Calibri 26",fg="black")
+y.pack(side=LEFT)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn=Button(window,text="Решить",font="Calibri 26",bg="white",command=handler)
+btn.pack(side=LEFT)
+btn_g=Button(window,text="График",font="Calibri 26",bg="white",command=graafik)
+btn_g.pack(side=LEFT)
 
 window.mainloop()
